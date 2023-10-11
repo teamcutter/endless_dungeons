5a86e318c348300e2097e7a88003407fb477e16b
teamcutter <ruslan.sibgatulin05@gmail.com>
2023-10-11 20:45:13 +0600

    added TileLayer

--- include/Map/TileLayer.hpp
+++ include/Map/TileLayer.hpp
@@ -1,5 +1,20 @@
 #pragma once
+#include "Layer.hpp"
+#include <string>
+#include <vector>
+
+struct Tileset {
+    int First, LastID, RowCount, ColCount, TileCount, TileSize;
+    std::string Name, Src;
+};
+
+using TileSet = std::vector<Tileset>;
+using TileMap = std::vector<std::vector<int>>;
 
 class TileLayer {
-    
+private:
+    int _titleSize, _rowCount, _colCount;
+
+public:
+    TileLayer(){}
 };
